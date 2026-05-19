c1946b2f74300b25d05be5bef019d199c1804be1
Stephanie Smithson <stephaniesmithson16@gmail.com>
2026-05-18 21:26:05 -0500

    Add offset to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
 
+const PAGE_SIZE = 25;
+
 type Inspection = {
-  inspection_id: string;
+  inspection_id: number;
   name: string;
+  aka_name: string;
+  license: string;
+  facility_type: string;
   address: string;
   zip_code: string;
   risk: string;
   results: string;
   inspection_date: string;
+  inspection_type: string;
+  violations: string;
 };
 
 function App() {
   const [inspections, setInspections] = useState<Inspection[]>([]);
   const [loading, setLoading] = useState(true);
+  const [offset, setOffset] = useState<number>(0);
 
   useEffect(() => {
     async function fetchInspections() {
       try {
+        setLoading(true);
+
         const response = await fetch(
-          "http://127.0.0.1:8000/api/v1/restaurants/inspections?limit=25"
+          `http://127.0.0.1:8000/api/v1/restaurants/inspections?limit=${PAGE_SIZE}&offset=${offset}`,
         );
 
         const data = await response.json();
@@ -32,18 +42,21 @@ function App() {
     }
 
     fetchInspections();
-  }, []);
+  }, [offset]);
 
   return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Chicago Smart City Dashboard</h1>
+    <main
+      style={{ padding: "2rem", fontFamily: "sans-serif", fontSize: "16px" }}
+    >
+      <h1>Chicago Restaurant Inspections</h1>
 
       {loading ? (
         <p>Loading inspections...</p>
       ) : (
         <table
           style={{
-            borderCollapse: "collapse",
+            borderCollapse: "separate",
+            borderColor: "#ddd",
             width: "100%",
             marginTop: "2rem",
           }}
@@ -51,10 +64,14 @@ function App() {
           <thead>
             <tr>
               <th>Name</th>
+              <th>Aka Name</th>
+              <th>License</th>
+              <th>Facility Type</th>
               <th>Address</th>
               <th>Zip</th>
               <th>Risk</th>
               <th>Result</th>
+              <th>Type</th>
               <th>Date</th>
             </tr>
           </thead>
@@ -63,16 +80,51 @@ function App() {
             {inspections.map((inspection) => (
               <tr key={inspection.inspection_id}>
                 <td>{inspection.name}</td>
+                <td>{inspection.aka_name}</td>
+                <td>{inspection.license}</td>
+                <td>{inspection.facility_type}</td>
                 <td>{inspection.address}</td>
                 <td>{inspection.zip_code}</td>
                 <td>{inspection.risk}</td>
                 <td>{inspection.results}</td>
+                <td>{inspection.inspection_type}</td>
                 <td>{inspection.inspection_date}</td>
               </tr>
             ))}
           </tbody>
         </table>
       )}
+      <div style={{ display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "2rem",
+        gap: "2rem"}}>
+        <button
+          style={{
+            padding: "0.5rem 1rem",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            setOffset((currentOffset) => Math.max(currentOffset - PAGE_SIZE, 0));
+          }}
+          disabled={offset === 0}
+        >
+          Back
+        </button>
+        <button
+          style={{
+            padding: "0.5rem 1rem",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            setOffset((currentOffset) => currentOffset + PAGE_SIZE);
+          }}
+        >
+          Next
+        </button>
+      </div>
     </main>
   );
 }
